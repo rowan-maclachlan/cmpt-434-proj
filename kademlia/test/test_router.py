5cb6d3702d78fb7552bc6d91db63080ab487321a
Rowan MacLachlan (rdm695) <rdm695@mail.usask.ca>
2019-03-28 20:55:08 -0600

    test for find_nearest_neighbours

--- kademlia/test/test_router.py
+++ kademlia/test/test_router.py
@@ -8,7 +8,7 @@ from kademlia.KBucket import KBucket
 from kademlia.Contact import Contact
 import kademlia.params as p 
 
-def test_router_get_bucket():
+def test_get_bucket():
     b = p.params[p.B]
     k = p.params[p.K]
     ip = "127.0.0.1"
@@ -42,7 +42,7 @@ def test_router_get_bucket():
     assert len(table.get_bucket(contact4.getId())) == 1
 
 
-def test_router_wont_add_self():
+def test_wont_add_self():
     b = p.params[p.B]
     k = p.params[p.K]
     ip = "127.0.0.1"
@@ -57,7 +57,7 @@ def test_router_wont_add_self():
     assert len(table) == 0
 
 
-def test_router_add_contact():
+def test_add_contact():
     b = p.params[p.B]
     k = p.params[p.K]
     ip = "127.0.0.1"
@@ -74,4 +74,48 @@ def test_router_add_contact():
     assert table.add_contact(contact2)
     assert table.add_contact(contact3)
     assert table.add_contact(contact4)
+
+
+def test_find_nearest_neighbours():
+    """
+    Add 8 contacts to the routing table.
+    Assert that when we call find nearest with some ID, we get 4 of those
+    contacts which are nearest to the function's argument.
+    """
+
+    k = 4
+    ip = "127.0.0.1"
+    port = 1234
+
+    contact1 = Contact(1, ip, port)
+    contact2 = Contact(2, ip, port)
+    contact3 = Contact(3, ip, port)
+    contact4 = Contact(4, ip, port)
+    contact5 = Contact(8, ip, port)
+    contact6 = Contact(9, ip, port)
+    contact7 = Contact(10, ip, port)
+    contact8 = Contact(11, ip, port)
+
+    table = RoutingTable(4, k, 0)
+
+    assert table.add_contact(contact1)
+    assert table.add_contact(contact2)
+    assert table.add_contact(contact3)
+    assert table.add_contact(contact4)
+    assert table.add_contact(contact5)
+    assert table.add_contact(contact6)
+    assert table.add_contact(contact7)
+    assert table.add_contact(contact8)
+
+    nearest = table.find_nearest_neighbours(12)
+    assert len(nearest) == 4
+
+    assert contact8 in nearest
+    assert contact7 in nearest
+    assert contact6 in nearest
+    assert contact5 in nearest
+    assert contact4 not in nearest
+    assert contact3 not in nearest
+    assert contact2 not in nearest
+    assert contact1 not in nearest
 
