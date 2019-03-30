902ca46cf0ddf9af87a80d1f9171cab344e1d849
Rowan MacLachlan (rdm695) <rdm695@mail.usask.ca>
2019-03-29 18:39:45 -0600

    Trying to get IO working asynchronously

--- kad.py
+++ kad.py
@@ -4,6 +4,48 @@ import asyncio
 
 from kademlia.Node import Node
 
+################################################################################
+async def do_get(node, key):
+    result = await node.get(key)
+    if result is None:
+        print("No such key-value exists on the network.")
+    else:
+        print(result[1] if result[0] else "No response received from peers.")
+
+async def do_set(node, key, value):
+    result = await node.put(key, value)
+    print(result[1] if result[0] else "No response received.")
+
+async def do_ping(node, ip, port):
+    result = await node.ping(ip, int(port))
+    print(result[1] if result[0] else "No response received.")
+
+################################################################################
+def handle_input(node):
+    command = ""
+    args = ""
+    instructions = "'set <key (str)> <value (str)>' to store data\n"\
+                   "'get <value (str)>' to retrieve data\n"\
+                   "'quit' to leave\n"
+    args = sys.stdin.readline().split(" ")
+    loop = asyncio.get_event_loop()
+    
+    if args[0] == "get":
+        print(f"do get {args[1]}...")
+        loop.run_until_complete(do_get(node, args[1]))
+    elif args[0] == "set":
+        print(f"do set {args[1]} {args[2]}...")
+        loop.run_until_complete(do_set(node, args[1], args[2]))
+    elif args[0] == "ping":
+        loop.run_until_complete(do_ping(node, args[1], args[2]))
+    elif args[0] == "inspect":
+        print(str(node.table))
+    elif args[0] == "quit":
+        print("Leaving!")
+    else:
+        print("Invalid command.  Try again.")
+################################################################################
+
 if len(sys.argv) == 3:
     my_ip = sys.argv[1]
     my_port = sys.argv[2]
@@ -43,5 +85,9 @@ loop.set_debug(True)
 # Create Kademlia node
 node = Node(my_ip, my_port)
 
+print("This process stores and retrieves strings on a"\
+      " distributed hash table based off of the Kademlia protocol.")
+
 loop.run_until_complete(node.listen())
+loop.add_reader(sys.stdin, handle_input, node, loop)
 loop.run_forever()
